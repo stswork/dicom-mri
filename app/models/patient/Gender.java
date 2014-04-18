6c03b27b046d0437c2f2633b385169b2cb1c233c
Sagar Gopale <sagar090989@yahoo.com>
2014-04-18 20:42:37 +0530

    Recreated repo

---
+++ app/models/patient/Gender.java
@@ -0,0 +1,12 @@
+package models.patient;
+
+import com.avaje.ebean.annotation.EnumValue;
+
+/**
+ * Created by Sagar Gopale on 3/8/14.
+ */
+public enum Gender {
+    MALE,
+    FEMALE,
+    DONT_WANT_TO_SAY
+}

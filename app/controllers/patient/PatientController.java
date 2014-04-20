be6c911e11050ac53b1169220abca1175e1ee7d8
Shubho Pramanik <shubho.pramanik@gmail.com>
2014-04-20 15:44:08 +0530

    Review ID

--- app/controllers/patient/PatientController.java
+++ app/controllers/patient/PatientController.java
@@ -255,7 +255,7 @@ public class PatientController extends Controller {
                 }
                 try {
 
-                    String url= TELESTROKE_URL +"?username="+user.getUserName()+"&password="+user.getPassword()+"&id="+p.getId();
+                    String url= TELESTROKE_URL +"?username="+user.getUserName()+"&password="+user.getPassword()+"&id="+r.getId();
 
                     //SENDING REGISTRATION SMS
                     String exotelSmsBody = "Message from Telestroke, \nNew patient details " + url + "\nfrom  " + u.getDisplayName();
